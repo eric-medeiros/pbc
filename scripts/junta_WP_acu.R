65a576dda4057c72b1359e447a4eac387c9e0e05
eric-medeiros <ericmedeiros@gmail.com>
2020-07-24 18:53:05 -0300

    Importação da nova tabela de acústica

--- scripts/junta_WP_acu.R
+++ scripts/junta_WP_acu.R
@@ -19,7 +19,18 @@ list_acu$saidas <- list_acu$saidas %>%
   )
 
 list_acu$saidas <- list_acu$saidas %>%
-  dplyr::select(saida, data, barco, datahora_I, lng_I, lat_I, WP_I, area, equipe, litros_abastecidos, OBS, data_WP_F_saida)
+  dplyr::select(saida,
+                data,
+                barco,
+                datahora_I,
+                lng_I,
+                lat_I,
+                WP_I,
+                periodo,
+                equipe,
+                litros_abastecidos,
+                OBS,
+                data_WP_F_saida)
 
 # Acima foi trocado os WPs iniciais e abaixo os WPs finais.
 
@@ -35,7 +46,21 @@ list_acu$saidas <- list_acu$saidas %>%
   )
 
 list_acu$saidas <- list_acu$saidas %>%
-  dplyr::select(saida, data, barco, datahora_I, lng_I, lat_I, WP_I, datahora_F, lng_F, lat_F, WP_F, area, equipe, litros_abastecidos, OBS)
+  dplyr::select(saida,
+                data,
+                barco,
+                datahora_I,
+                lng_I,
+                lat_I,
+                WP_I,
+                datahora_F,
+                lng_F,
+                lat_F,
+                WP_F,
+                periodo,
+                equipe,
+                litros_abastecidos,
+                OBS)
 
 # Ajuste de dos tipos de variáveis de saidas
 list_acu$saidas$datahora_I <- ymd_hms(list_acu$saidas$datahora_I)
@@ -61,7 +86,20 @@ list_acu$clima <- list_acu$clima %>%
   )
 
 list_acu$clima <- list_acu$clima %>%
-  dplyr::select(saida, datahora_I, lng_I, lat_I, WP_I, dir_vento, veloc_vento, beaufort, cobert_nuvens, visibilidade, reflexo, coloracao_agua, OBS, data_WP_F_clima)
+  dplyr::select(saida,
+                datahora_I,
+                lng_I,
+                lat_I,
+                WP_I,
+                dir_vento,
+                veloc_vento,
+                beaufort,
+                cobert_nuvens,
+                visibilidade,
+                reflexo,
+                coloracao_agua,
+                OBS,
+                data_WP_F_clima)
 
 # Acima foi trocado os WPs iniciais e abaixo os WPs finais.
 
@@ -77,7 +115,23 @@ list_acu$clima <- list_acu$clima %>%
   )
 
 list_acu$clima <- list_acu$clima %>%
-  dplyr::select(saida, datahora_I, lng_I, lat_I, WP_I, datahora_F, lng_F, lat_F, WP_F, dir_vento, veloc_vento, beaufort, cobert_nuvens, visibilidade, reflexo, coloracao_agua, OBS)
+  dplyr::select(saida, 
+                datahora_I, 
+                lng_I, 
+                lat_I, 
+                WP_I, 
+                datahora_F, 
+                lng_F, 
+                lat_F, 
+                WP_F, 
+                dir_vento, 
+                veloc_vento, 
+                beaufort, 
+                cobert_nuvens,
+                visibilidade,
+                reflexo,
+                coloracao_agua,
+                OBS)
 
 # Ajuste de dos tipos de variáveis de clima
 list_acu$clima$datahora_I <- ymd_hms(list_acu$clima$datahora_I)
@@ -87,14 +141,14 @@ list_acu$clima$datahora_F <- ymd_hms(list_acu$clima$datahora_F)
 list_acu$clima$lng_F <- as.numeric(list_acu$clima$lng_F)
 list_acu$clima$lat_F <- as.numeric(list_acu$clima$lat_F)
 
-# O mesmo para gravacoes
-list_acu$gravacoes$data_WP_I <- str_c(as.character(list_acu$gravacoes$data),"-", list_acu$gravacoes$WP_I_grupo)
-list_acu$gravacoes$data_WP_F <- str_c(as.character(list_acu$gravacoes$data),"-", list_acu$gravacoes$WP_F_grupo)
+# O mesmo para WPs de estacoes
+list_acu$estacoes$data_WP_I_estacao <- str_c(as.character(list_acu$estacoes$data),"-", list_acu$estacoes$WP_I_estacao)
+list_acu$estacoes$data_WP_F_estacao <- str_c(as.character(list_acu$estacoes$data),"-", list_acu$estacoes$WP_F_estacao)
 
-list_acu$gravacoes <- list_acu$gravacoes %>%
-  left_join(dados_WP, by= c("data_WP_I" = "data_WP_WP"))
+list_acu$estacoes <- list_acu$estacoes %>%
+  left_join(dados_WP, by= c("data_WP_I_estacao" = "data_WP_WP"))
 
-list_acu$gravacoes <- list_acu$gravacoes %>%
+list_acu$estacoes <- list_acu$estacoes %>%
   rename(
     datahora_I = datahora_WP,
     lng_I = lng_WP,
@@ -102,15 +156,22 @@ list_acu$gravacoes <- list_acu$gravacoes %>%
     WP_I = WP_WP
   )
 
-list_acu$gravacoes <- list_acu$gravacoes %>%
-  dplyr::select(saida, data, grupo, datahora_I, lng_I, lat_I, WP_I, arquivo, profund_m, estado, coesao, tam_grupo, tam_min, tam_max, n_neonatos, n_infantes, n_juvenis, n_adultos, quant_embarc, OBS, data_WP_F)
+list_acu$estacoes <- list_acu$estacoes %>%
+  dplyr::select(saida,
+                estacao,
+                datahora_I,
+                lng_I,
+                lat_I,
+                WP_I,
+                OBS,
+                data_WP_F_estacao)
 
-# Está funcionando, mas ficou confuso. Acima foi trocado os WPs iniciais e abaixo os WPs finais.
+# Acima foi trocado os WPs iniciais e abaixo os WPs finais.
 
-list_acu$gravacoes <- list_acu$gravacoes %>%
-  left_join(dados_WP, by= c("data_WP_F" = "data_WP_WP"))
+list_acu$estacoes <- list_acu$estacoes %>%
+  left_join(dados_WP, by= c("data_WP_F_estacao" = "data_WP_WP"))
 
-list_acu$gravacoes <- list_acu$gravacoes %>% 
+list_acu$estacoes <- list_acu$estacoes %>% 
   rename(
     datahora_F = datahora_WP,
     lng_F = lng_WP,
@@ -118,16 +179,175 @@ list_acu$gravacoes <- list_acu$gravacoes %>%
     WP_F = WP_WP
   )
 
+list_acu$estacoes <- list_acu$estacoes %>%
+  dplyr::select(saida,
+                estacao,
+                datahora_I,
+                lng_I,
+                lat_I,
+                WP_I,
+                datahora_F,
+                lng_F,
+                lat_F,
+                OBS)
+
+# Ajuste de dos tipos de variáveis de estacoes
+list_acu$estacoes$datahora_I <- ymd_hms(list_acu$estacoes$datahora_I)
+list_acu$estacoes$lng_I <- as.numeric(list_acu$estacoes$lng_I)
+list_acu$estacoes$lat_I <- as.numeric(list_acu$estacoes$lat_I)
+list_acu$estacoes$datahora_F <- ymd_hms(list_acu$estacoes$datahora_F)
+list_acu$estacoes$lng_F <- as.numeric(list_acu$estacoes$lng_F)
+list_acu$estacoes$lat_F <- as.numeric(list_acu$estacoes$lat_F)
+
+# O mesmo para gravacoes
+list_acu$gravacoes$data_WP_arq <- str_c(as.character(list_acu$gravacoes$data),"-", list_acu$gravacoes$WP_arq)
+
 list_acu$gravacoes <- list_acu$gravacoes %>%
-  dplyr::select(saida, data, grupo, datahora_I, lng_I, lat_I, WP_I, datahora_F, lng_F, lat_F, WP_F, arquivo, profund_m, estado, coesao, tam_grupo, tam_min, tam_max, n_neonatos, n_infantes, n_juvenis, n_adultos, quant_embarc, OBS)
+  left_join(dados_WP, by= c("data_WP_arq" = "data_WP_WP"))
+
+list_acu$gravacoes <- list_acu$gravacoes %>%
+  rename(
+    datahora = datahora_WP,
+    lng = lng_WP,
+    lat = lat_WP,
+    WP = WP_WP
+  )
+
+list_acu$gravacoes <- list_acu$gravacoes %>%
+  dplyr::select(saida,
+                data,
+                area,
+                estacao,
+                arq,
+                datahora,
+                lng,
+                lat,
+                WP,
+                arq_wav,
+                profund_m,
+                OBS)
 
 # Ajuste de dos tipos de variáveis de gravacoes
-list_acu$gravacoes$datahora_I <- ymd_hms(list_acu$gravacoes$datahora_I)
-list_acu$gravacoes$lng_I <- as.numeric(list_acu$gravacoes$lng_I)
-list_acu$gravacoes$lat_I <- as.numeric(list_acu$gravacoes$lat_I)
-list_acu$gravacoes$datahora_F <- ymd_hms(list_acu$gravacoes$datahora_F)
-list_acu$gravacoes$lng_F <- as.numeric(list_acu$gravacoes$lng_F)
-list_acu$gravacoes$lat_F <- as.numeric(list_acu$gravacoes$lat_F)
+list_acu$gravacoes$datahora <- ymd_hms(list_acu$gravacoes$datahora)
+list_acu$gravacoes$lng <- as.numeric(list_acu$gravacoes$lng)
+list_acu$gravacoes$lat <- as.numeric(list_acu$gravacoes$lat)
+
+# O mesmo para WPs de grupos
+list_acu$grupos$data_WP_I_G <- str_c(as.character(list_acu$grupos$data),"-", list_acu$grupos$WP_I_G)
+list_acu$grupos$data_WP_F_G <- str_c(as.character(list_acu$grupos$data),"-", list_acu$grupos$WP_F_G)
+
+list_acu$grupos <- list_acu$grupos %>%
+  left_join(dados_WP, by= c("data_WP_I_G" = "data_WP_WP"))
+
+list_acu$grupos <- list_acu$grupos %>%
+  rename(datahora_I = datahora_WP,
+         lng_I = lng_WP,
+         lat_I = lat_WP,
+         WP_I = WP_WP)
+
+list_acu$grupos <- list_acu$grupos %>%
+  dplyr::select(saida, 
+                data, 
+                estacao, 
+                grupo, 
+                arq_wav, 
+                datahora_I, 
+                lng_I, 
+                lat_I, 
+                WP_I, 
+                estado, 
+                coesao, 
+                tam_grupo, 
+                tam_min, 
+                tam_max, 
+                n_neonatos, 
+                n_infantes, 
+                n_juvenis, 
+                n_adultos, 
+                OBS, 
+                data_WP_F_G)
+
+# Acima foi trocado os WPs iniciais e abaixo os WPs finais.
+
+list_acu$grupos <- list_acu$grupos %>%
+  left_join(dados_WP, by= c("data_WP_F_G" = "data_WP_WP"))
+
+list_acu$grupos <- list_acu$grupos %>% 
+  rename(datahora_F = datahora_WP,
+         lng_F = lng_WP,
+         lat_F = lat_WP,
+         WP_F = WP_WP)
+
+list_acu$grupos <- list_acu$grupos %>%
+  dplyr::select(saida,
+                data,
+                estacao,
+                grupo,
+                arq_wav,
+                datahora_I,
+                lng_I,
+                lat_I,
+                WP_I,
+                datahora_F,
+                lng_F,
+                lat_F,
+                estado,
+                coesao,
+                tam_grupo,
+                tam_min,
+                tam_max,
+                n_neonatos,
+                n_infantes,
+                n_juvenis,
+                n_adultos,
+                OBS)
+   
+# Ajuste de dos tipos de variáveis de grupos
+list_acu$grupos$datahora_I <- ymd_hms(list_acu$grupos$datahora_I)
+list_acu$grupos$lng_I <- as.numeric(list_acu$grupos$lng_I)
+list_acu$grupos$lat_I <- as.numeric(list_acu$grupos$lat_I)
+list_acu$grupos$datahora_F <- ymd_hms(list_acu$grupos$datahora_F)
+list_acu$grupos$lng_F <- as.numeric(list_acu$grupos$lng_F)
+list_acu$grupos$lat_F <- as.numeric(list_acu$grupos$lat_F)
+
+# O mesmo para WPs de embarcacoes
+list_acu$embarcacoes$data_WP_emb <- str_c(as.character(list_acu$embarcacoes$data),"-", list_acu$embarcacoes$WP_emb)
+
+list_acu$embarcacoes <- list_acu$embarcacoes %>%
+  left_join(dados_WP, by= c("data_WP_emb" = "data_WP_WP"))
+
+list_acu$embarcacoes <- list_acu$embarcacoes %>%
+  rename(datahora = datahora_WP,
+         lng = lng_WP,
+         lat = lat_WP,
+         WP = WP_WP)
+
+list_acu$embarcacoes <- list_acu$embarcacoes %>%
+  dplyr::select(saida,
+                data,
+                estacao,
+                arq,
+                emb,
+                datahora,
+                lng,
+                lat,
+                WP,
+                ID_emb,
+                tipo_emb,
+                finalid_emb,
+                quant_emb,
+                potenc_hp_motor,
+                tipo_motor,
+                registro_na_gravacao,
+                distancia_m,
+                OBS)
+
+# Ajuste de dos tipos de variáveis de embarcacoes
+list_acu$embarcacoes$datahora <- ymd_hms(list_acu$embarcacoes$datahora)
+list_acu$embarcacoes$lng <- as.numeric(list_acu$embarcacoes$lng)
+list_acu$embarcacoes$lat <- as.numeric(list_acu$embarcacoes$lat)
+
+invisible(list_acu)
 
 return(list_acu)
 }
